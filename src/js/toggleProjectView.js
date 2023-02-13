2dd9b5cb73233505c9a6421e798e23f3b977f834
Emil Backlund <emil_backi_@hotmail.com>
2023-02-13 22:22:08 +0100

    add some accessibility and minimize the size of images

--- src/js/toggleProjectView.js
+++ src/js/toggleProjectView.js
@@ -4,12 +4,15 @@ const overlay = document.querySelectorAll('.overlay');
 
 for (let i = 0; i < viewButtonsContainer.length; i += 1) {
   viewButtonsContainer[i].addEventListener('click', (e) => {
-    console.log(e.target.nodeName);
     if (e.target.nodeName === 'BUTTON') {
       viewButtonsContainer[i].stopPropagation();
     }
     viewButtons[i].classList.toggle('computer:flex');
     overlay[i].classList.toggle('computer:block');
     viewButtonsContainer[i].classList.toggle('computer:ring-2');
+  });
+  viewButtonsContainer[i].addEventListener('focus', (e) => {
+    viewButtons[i].classList.add('computer:flex');
+    overlay[i].classList.add('computer:block');
   });
 }
