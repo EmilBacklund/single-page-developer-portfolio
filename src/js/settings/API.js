f422a8ad8130ce362b947f21c04bcd23a750d84e
Emil Backlund <emil_backi_@hotmail.com>
2023-07-02 22:45:39 +0200

    almost done

--- src/js/settings/API.js
+++ src/js/settings/API.js
@@ -1,3 +1,4 @@
 const SUBMIT_ENDPOINT = 'https://api.airtable.com/v0/app5p0pMLnJNqDmKc/Contact%20Messages/';
+const GET_PROJECTS = 'https://api.airtable.com/v0/app5p0pMLnJNqDmKc/projects';
 
-export default SUBMIT_ENDPOINT;
+export { SUBMIT_ENDPOINT, GET_PROJECTS };
